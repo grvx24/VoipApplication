55dd00cb11aab1cbb8871b4f3d48a2f8bd6435e6
Kamil Sagalara <grevax24@gmail.com>
2018-08-08 16:01:26 +0200

    Autorzy

--- VoipApplication/Client/GuideUserControl.xaml.cs
+++ VoipApplication/Client/GuideUserControl.xaml.cs
@@ -13,20 +13,6 @@ namespace VoIP_Client
             this.client = client;
             this.callingService = callingService;
             InitializeComponent();
-            GuideTextBlock.Text = GuideText();
-        }
-        private static string GuideText()
-        {
-            return
-                  "Ustawienia:\tumożliwia zmianę adresu email, zmianę hasła, wybór mikrofonu.\n\n" +
-                  "Użytkownicy:\t\n\n" +
-                  "\tWyszukaj:\tumożliwia wyszukiwanie użytkowników na serwerze.\n\n" +
-                  "\tOnline:\t\tpokazuje aktualnie zalogowanych uzytkowników.\n\n" +
-                  "\tUlubione:\tpokazuje ulubionych użytkowników.\n\n" +
-                  "\tZadzwoń:\tnawiązuje połączenie głosowe z danym użytkownikiem.\n\n" +
-                  "\tEdytuj:\t\tumożliwia dodanie użytkownika do ulubionych po podaniu nazwy, zmianę tej nazwy lub usuniecie z ulubionych po usunięciu nazwy.\n\n" +
-                  "Instrukcja:\tzawiera skrót funkcji aplikacji.\n\n" +
-                  "Wyloguj się:\twylogowuje użytkownika.";
         }
     }
 }
